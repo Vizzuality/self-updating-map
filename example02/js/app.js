299abea9670bdd741c7ac2a1a938a3f4aa5b69da
Javier de la Torre <jatorre@gmail.com>
2012-10-17 20:58:50 +0200

    added a hack to support multiple domains

--- example02/js/app.js
+++ example02/js/app.js
@@ -101,7 +101,7 @@ var tileStyleData = "#st0921md{line-width:1;line-opacity:1;}[status='none']{line
 
       extra_params: { update: updatedAt },
 
-      tiler_domain: 'd2f7e17k9sr9mq.cloudfront.net',
+      tiler_domain: "{s}.wsjgraphics.cartodb.com",
 
       featureOver:  onHover,
       featureOut:   onOut,
